4ba520675114a2c6138c1f22d71ca5e633f5c4b6
Jakub Vyvazil <jakub@vyvazil.cz>
2018-12-01 20:11:20 +0100

    prepared encore with bootstrap | bootswatch

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,9 +6,23 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
+require('../sass/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+require('underscore');
+require('backbone');
+var $ = require('jquery');
+// JS is equivalent to the normal "bootstrap" package
+// no need to set this to a variable, just require it
+require('tether');
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap-sass/javascripts/bootstrap/tooltip');
+// require('bootstrap-sass/javascripts/bootstrap/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
